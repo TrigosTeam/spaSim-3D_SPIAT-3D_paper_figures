603273d777507216aaa95c09ebbd76b1bb54a252
David Le <dle@nid002047.hostmgmt.cm.setonix.pawsey.org.au>
2025-07-16 14:25:07 +0800

    Started working on figures for clustering algorithms.

--- figure_1B_SPIAT3D_workflow.R
+++ figure_1B_SPIAT3D_workflow.R
@@ -128,6 +128,9 @@ trace_circles
 
 ### 2. Spatial heterogeneity metrics plots ----
 # Alter plot_cells3D function
+
+
+# 2.1. Simulation ----
 plot_cells3D <- function(spe,
                          plot_cell_types = NULL,
                          plot_colours = NULL,
@@ -220,7 +223,6 @@ plot_cells3D <- function(spe,
 }
 
 
-# 2.1. Simulation ----
 bg_metadata <- spe_metadata_background_template("random")
 bg_metadata$background$n_cells <- 10000
 bg_metadata$background$length <- 500
@@ -420,3 +422,135 @@ calculate_cell_proportion_grid_metrics3D(spe_cluster, 10, 'A', 'B')
 
 ### 3. Cell clustering algorithms plots ----
 
+# 3.1. Simulation ----
+# Alter plot_cells3D function
+plot_cells3D <- function(spe,
+                         plot_cell_types = NULL,
+                         plot_colours = NULL,
+                         feature_colname = "Cell.Type") {
+  
+  # Check input parameters
+  if (class(spe) != "SpatialExperiment") {
+    stop("`spe` is not a SpatialExperiment object.")
+  }
+  if (!is.null(plot_cell_types) && !is.character(plot_cell_types)) {
+    stop("`plot_cell_types` is not a character vector or NULL.")
+  } 
+  if (!is.null(plot_colours) && !is.character(plot_colours)) {
+    stop("`plot_colours` is not a character vector or NULL.")
+  } 
+  if (is.character(plot_colours)) {
+    non_colours <- plot_colours[which(!(sapply(plot_colours, function(X) {
+      tryCatch(is.matrix(col2rgb(X)), 
+               error = function(e) FALSE)
+    })))]
+    if (length(non_colours) > 0) {
+      stop(paste("The following plot_colours are not colours:\n   ",
+                 paste(non_colours, collapse = ", ")))
+    } 
+  }
+  if (!is.character(feature_colname)) {
+    stop("`feature_colname` is not a character.")
+  }
+  if (is.null(spe[[feature_colname]])) {
+    stop(paste(feature_colname, "is not a valid column in your spe object."))
+  }
+  
+  ## Convert spe object to data frame
+  df <- data.frame(spatialCoords(spe), "Cell.Type" = spe[[feature_colname]])
+  
+  ## If no cell types chosen, use all cell types found in data frame
+  if (is.null(plot_cell_types)) {
+    warning("plot_cell_types not specified, all cell types found in the spe object will be used.")
+    plot_cell_types <- unique(df[["Cell.Type"]])
+  }
+  ## If no colours inputted, use rainbow palette
+  if (is.null(plot_colours)) {
+    warning("plot_colours not specified, rainbow palette will be used.")
+    plot_colours <- rainbow(length(plot_cell_types))
+  }
+  ## User inputs mismatching cell types and colours
+  if (length(plot_cell_types) != length(plot_colours)) {
+    stop("Length of plot_cell_types is not equal to length of plot_colours")
+  }
+  
+  ## If cell types have been chosen, check they are found in the spe object
+  spe_cell_types <- unique(spe[[feature_colname]])
+  unknown_cell_types <- setdiff(plot_cell_types, spe_cell_types)
+  
+  if (length(unknown_cell_types) == length(plot_cell_types)) {
+    stop("None of the plot_cell_types are found in the spe object")
+  }
+  
+  if (length(unknown_cell_types) != 0) {
+    warning(paste("The following plot_cell_types are not found in the spe object:\n   ",
+                  paste(unknown_cell_types, collapse = ", ")))
+    plot_colours <- plot_colours[which(plot_cell_types %in% spe_cell_types)]
+    plot_cell_types <- intersect(plot_cell_types, spe_cell_types)
+  }
+  
+  ## Factor for feature column
+  df[, "Cell.Type"] <- factor(df[, "Cell.Type"],
+                              levels = plot_cell_types)
+  
+  ## Plot
+  fig <- plot_ly(df,
+                 type = "scatter3d",
+                 mode = 'markers',
+                 x = ~Cell.X.Position,
+                 y = ~Cell.Y.Position,
+                 z = ~Cell.Z.Position,
+                 color = ~Cell.Type,
+                 colors = plot_colours,
+                 marker = list(size = 2))
+  
+  fig <- fig %>% layout(scene = list(xaxis = list(title = '', showgrid = F, showaxeslabels = F, showticklabels = F, 
+                                                  titlefont = list(size = 20), tickfont = list(size = 15), range = c(0, 500)),
+                                     yaxis = list(title = '', showgrid = F, showaxeslabels = F, showticklabels = F,
+                                                  titlefont = list(size = 20), tickfont = list(size = 15), range = c(0, 500)),
+                                     zaxis = list(title = '', showgrid = F, showaxeslabels = F, showticklabels = F,
+                                                  titlefont = list(size = 20), tickfont = list(size = 15), range = c(0, 500)),
+                                     aspectmode = "cube"))
+  
+  return(fig)
+}
+
+
+bg_metadata <- spe_metadata_background_template("random")
+cluster_metadata <- spe_metadata_cluster_template("ring", "ellipsoid", bg_metadata)
+cluster_metadata <- spe_metadata_cluster_template("regular", "network", cluster_metadata)
+
+cluster_metadata$background$n_cells <- 10000
+cluster_metadata$background$length <- 500
+cluster_metadata$background$width <- 500
+cluster_metadata$background$height <- 500
+cluster_metadata$background$minimum_distance_between_cells <- 10
+cluster_metadata$background$cell_types <- c('A', 'B', 'O')
+cluster_metadata$background$cell_proportions <- c(0.01, 0.01, 0.98)
+
+cluster_metadata$cluster_1$cluster_cell_types <- c('A', 'B')
+cluster_metadata$cluster_1$cluster_cell_proportions <- c(0.8, 0.2)
+cluster_metadata$cluster_1$x_radius <- 150
+cluster_metadata$cluster_1$x_radius <- 150
+cluster_metadata$cluster_1$x_radius <- 200
+cluster_metadata$cluster_1$x_y_rotation <- 0
+cluster_metadata$cluster_1$x_z_rotation <- 30
+cluster_metadata$cluster_1$y_z_rotation <- -45
+cluster_metadata$cluster_1$centre_loc <- c(400, 125, 125)
+cluster_metadata$cluster_1$ring_cell_types <- c('A', 'B')
+cluster_metadata$cluster_1$ring_cell_proportions <- c(0, 1)
+cluster_metadata$cluster_1$ring_width <- 25
+
+cluster_metadata$cluster_2$cluster_cell_types <- c('A', 'B')
+cluster_metadata$cluster_2$cluster_cell_proportions <- c(0.8, 0.2)
+cluster_metadata$cluster_2$radius <- 200
+cluster_metadata$cluster_2$centre_loc <- c(150, 150, 400)
+cluster_metadata$cluster_2$n_edges <- 20
+cluster_metadata$cluster_2$width <- 50
+
+spe_cluster <- simulate_spe_metadata3D(cluster_metadata)
+plot_cells3D(spe_cluster, plot_cell_types = c('A', 'B'), plot_colours = c('orange', 'skyblue'))
+
+# 3.2. Clustering algorithm ----
+spe_ah <- alpha_hull_clustering3D(spe_cluster, 'A', 28, 100)
+plot_alpha_hull_clusters3D(spe_ah, plot_cell_types = c('A', 'B'), plot_colours = c('orange', 'skyblue'))
