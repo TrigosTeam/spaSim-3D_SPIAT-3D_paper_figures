87343b3331ee62ba06a6a478c4b1e6f5609855b2
David Le <dle@nid001070.hostmgmt.cm.setonix.pawsey.org.au>
2025-08-08 15:18:07 +0800

    Continued work on figure 1C.

--- figure_1C_procedure_to_generate_slices.R
+++ figure_1C_procedure_to_generate_slices.R
@@ -94,7 +94,7 @@ plot_cells3D <- function(spe,
 bg_metadata <- spe_metadata_background_template("random")
 cluster_metadata <- spe_metadata_cluster_template("regular", "ellipsoid", bg_metadata)
 
-cluster_metadata$background$n_cells <- 10000
+cluster_metadata$background$n_cells <- 50000
 cluster_metadata$background$length <- 500
 cluster_metadata$background$width <- 500
 cluster_metadata$background$height <- 500
@@ -113,14 +113,17 @@ cluster_metadata$cluster_1$y_z_rotation <- 0
 cluster_metadata$cluster_1$centre_loc <- c(250, 250, 250)
 
 spe_cluster <- simulate_spe_metadata3D(cluster_metadata)
-plot_cells3D(spe_cluster, plot_cell_types = c('A', 'B'), plot_colours = c('orange', 'skyblue'))
+plot_cells3D(spe_cluster, plot_cell_types = c('A', 'B'), plot_colours = c('#f77e3b', '#48bbff'))
 
 ### 2. Add slices to simulation ------------------------
 plot_cells3D_with_slices <- function(spe,
                                      plot_cell_types = NULL,
                                      plot_colours = NULL,
                                      feature_colname = "Cell.Type",
-                                     slice_positions) {
+                                     slice_positions,
+                                     slice_colors) {
+  
+  spatialCoords(spe) <- spatialCoords(spe) + 1000
   
   df <- data.frame(spatialCoords(spe_cluster), "Cell.Type" = spe_cluster[[feature_colname]])
   
@@ -148,36 +151,94 @@ plot_cells3D_with_slices <- function(spe,
                                                   titlefont = list(size = 20), tickfont = list(size = 15), range = c(0, 500)),
                                      aspectmode = "cube"))
   
-  vertices <- data.frame(x = c(0, 0, 500, 500, 0, 0, 500, 500),
-                         y = c(0, 500, 0, 500, 0, 500, 0, 500),
-                         z = c(50, 50, 50, 50, 100, 100, 100, 100))
-  
-  faces_temp <- data.frame(i = c(1, 4, 5, 8, 1, 6),
-                           j = c(2, 2, 6, 6, 5, 5),
-                           k = c(3, 3, 7, 7, 2, 2))
-  
-  fig <- fig %>%
-    add_trace(
-      type = 'mesh3d',
-      x = vertices[, 1], 
-      y = vertices[, 2], 
-      z = vertices[, 3],
-      i = faces_temp[, 1] - 1, 
-      j = faces_temp[, 2] - 1, 
-      k = faces_temp[, 3] - 1,
-      opacity = 0.5,
-      facecolor = rep("red", nrow(faces_temp))
-    )
+  index <- 1
+  for (slice_position in slice_positions) {
+    
+    vertices <- data.frame(x = c(0, 0, 500, 500, 0, 0, 500, 500),
+                           y = c(0, 500, 0, 500, 0, 500, 0, 500),
+                           z = rep(slice_position, each = 4))
+    
+    faces_temp <- data.frame(i = c(1, 4, 5, 8, 1, 6, 1, 1, 4, 4, 2, 2),
+                             j = c(2, 2, 6, 6, 5, 5, 3, 5, 8, 3, 4, 6),
+                             k = c(3, 3, 7, 7, 2, 2, 7, 7, 7, 7, 8, 8))
+    
+    fig <- fig %>%
+      add_trace(
+        type = 'mesh3d',
+        x = vertices[, 1], 
+        y = vertices[, 2], 
+        z = vertices[, 3],
+        i = faces_temp[, 1] - 1, 
+        j = faces_temp[, 2] - 1, 
+        k = faces_temp[, 3] - 1,
+        opacity = 0.5,
+        facecolor = rep(slice_colors[index], nrow(faces_temp))
+      )
+    
+    index <- index + 1
+  }
   
   methods::show(fig)
 }
 
-slice_positions_temp <- list(c(50, 100), c(150, 200), c(250, 300))
+slice_positions_temp <- list(c(335, 345), c(290, 300), c(245, 255))
 
 plot_cells3D_with_slices(spe_cluster, 
                          plot_cell_types = c('A', 'B'), 
-                         plot_colours = c('orange', 'skyblue'),
-                         slice_positions = slice_positions_temp)
+                         plot_colours = c('#f77e3b', '#48bbff'),
+                         slice_positions = slice_positions_temp,
+                         slice_colors = c("#b8db50",
+                                          "#73ec81",
+                                          "#9437a8"))
 
 
+
+
+
+
+### 3. Extract slices -----
+# Function to get slices from spe
+get_spes_for_slices <- function(spe, slice_positions) {
+  
+  spes_for_slices <- list()
+  
+  number_of_slices <- length(slice_positions)
+  
+  for (i in seq(number_of_slices)) {
+    bottom_z_coord_of_slice <- slice_positions[[i]][1]
+    top_z_coord_of_slice <- slice_positions[[i]][2]
+    z_coords_of_cells_in_spe <- spatialCoords(spe)[ , "Cell.Z.Position"]
+    spe_for_slice <- spe[, bottom_z_coord_of_slice < z_coords_of_cells_in_spe & z_coords_of_cells_in_spe < top_z_coord_of_slice]
+    spatialCoords(spe_for_slice) <- spatialCoords(spe_for_slice)[ , c("Cell.X.Position", "Cell.Y.Position")]
+    
+    spes_for_slices[[i]] <- spe_for_slice
+  }
+  return(spes_for_slices)
+}
+
+plot_cells2D <- function(spe_slices,
+                         plot_cell_types = NULL,
+                         plot_colours = NULL,
+                         feature_colname = "Cell.Type") {
+  
+  for (spe_slice in spe_slices) {
+    
+    df_slice <- data.frame(spatialCoords(spe_slice), "Cell.Type" = spe_slice[[feature_colname]])
+    
+    fig <- ggplot(df_slice, aes(x = Cell.X.Position, y = Cell.Y.Position, color = Cell.Type)) +
+      geom_point()
+    
+    methods::show(fig)
+  }
+}
+
+
+slice_positions_temp <- list(c(335, 345), c(290, 300), c(245, 255))
+
+spe_slices_temp <- get_spes_for_slices(spe_cluster, slice_positions_temp)
+
+plot_cells2D(spe_slices_temp,
+             plot_cell_types = NULL,
+             plot_colours = NULL,
+             feature_colname = "Cell.Type")
 
