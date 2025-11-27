99d886ce8637303cd9f9a00fa72726f25eb564ec
David Le <dle@nid001516.hostmgmt.cm.setonix.pawsey.org.au>
2025-11-27 14:34:18 +0800

    Adding subheadings to prepare for plotting.

---
+++ figure_methods_SPIAT-3D.R
@@ -0,0 +1,17 @@
+### Distance metrics ----
+
+### CIN and CINP ----
+
+### Entropy ----
+
+### Mixing score and normalised mixing score ----
+
+### Cross K-function ----
+
+### Cross G-function ----
+
+### Co-occurrence ----
+
+### Gradient-based metrics -----
+
+### Grid metrics ----
---
+++ figure_methods_spaSim-3D.R
@@ -0,0 +1,20 @@
+### Poisson point process ----
+
+### Background (empty and mixed) ----
+
+### Sphere ----
+
+### Ellipsoid transformations ----
+
+### Ellipsoid ----
+
+### Cylinder justification ----
+
+### Cylinder ----
+
+### Prim's algorithm ----
+
+### Network ----
+
+### Mixed, ringed and double ringed clusters ----
+
